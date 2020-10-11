93574391f93043f770663475e93252a65f6be684
David Giang <davidgiang.413@gmail.com>
2020-10-11 17:12:22 -0400

    First commit

---
+++ Anatomy Fitness/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Anatomy Fitness
+//
+//  Created by David Giang on 10/11/20.
+//  Copyright © 2020 David Giang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Test code")
+    }
+
+
+}
+
